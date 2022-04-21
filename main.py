c48e1547fa289169621c10aeec3e24512e16b51a
Carli9510 <99306256+Carli9510@users.noreply.github.com>
2022-04-21 00:46:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'JoostAbraham'
+        tl_admin_user = '@Charls9510'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -455,7 +455,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5378432318:AAGg25wtYoDG7uywPSXjA6x1YZbjOOyhBbE'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
